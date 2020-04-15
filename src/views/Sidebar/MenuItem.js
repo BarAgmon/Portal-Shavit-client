86c1ec75b303763aea59ae301e07d5912606d8e4
Maya Rachmani <maya3rm@gmail.com>
2020-04-15 15:33:53 +0300

    understand a litle bit hooks

--- src/views/Sidebar/MenuItem.js
+++ src/views/Sidebar/MenuItem.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {Icon, Menu} from "semantic-ui-react";
 import { Link } from 'react-router-dom';
 
-export default function MenuItem ({iconName, iconText, path}) {
+export default function MenuItem ({iconName, iconText, itemClicked, id}) {
     return (
-        <Menu.Item as='a'>
+        <Menu.Item as='a' to='Database' onClick={() => itemClicked(id)}>
             <Icon name={iconName} />
             {iconText}
         </Menu.Item>
--- src/views/Sidebar/Sidebar.js
+++ src/views/Sidebar/Sidebar.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import { Menu, Sidebar } from 'semantic-ui-react';
 import '../../Semantic-UI-CSS-master/semantic.min.css';
 import MenuItem from './MenuItem';
+import Database from "../DBsControl/HomePage";
 
 const SidebarApp = () => {
-    const items = [{key: 1, iconText: 'Backup & Restore', iconName: 'database'},
-                   {key: 2, iconText: 'Documentation', iconName: 'dochub'},
-                   {key: 3, iconText: 'Docker CLI', iconName: 'docker'},
-                   {key: 4, iconText: 'servers', iconName: 'server'},
-                   {key: 5, iconText: 'services', iconName: 'wheelchair'}];
+    const [selectedItem, setItem] = useState(0);
+
+    const items = [
+            {key: 1, iconText: 'Backup & Restore', iconName: 'database'},
+            {key: 2, iconText: 'Documentation', iconName: 'dochub'},
+            {key: 3, iconText: 'Docker CLI', iconName: 'docker'},
+            {key: 4, iconText: 'Servers', iconName: 'server'},
+            {key: 5, iconText: 'Services', iconName: 'wheelchair'}
+        ];
 
     return(
         <Sidebar
@@ -23,7 +28,11 @@ const SidebarApp = () => {
         >
             {items.map((item =>
                 <MenuItem
-                    key={item.key} iconName={item.iconName} iconText={item.iconText}
+                    key={item.key}
+                    iconName={item.iconName}
+                    iconText={item.iconText}
+                    id={item.key}
+                    itemClicked={(id) => setItem(id)}
                 />
             ))}
         </Sidebar>
