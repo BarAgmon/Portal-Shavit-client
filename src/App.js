756a488ff7357657bea80b7dc01e2fdd17835eca
Maya Rachmani <maya3rm@gmail.com>
2020-04-15 23:42:23 +0300

    added servers page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import Database from "./views/DBsControl/HomePage";
 import DatabaseBackup from "./views/DBsControl/BackupPage";
 import Links from "./views/Links/HomePage";
+import Servers from "./views/Servers/Servers";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
               <Switch>
                     <Route path="/backup" component={DatabaseBackup}/>
                     <Route path="/database" component={Database}/>
+                    <Route path="/Servers" component={Servers}/>
                     <Route path="/" component={Links}/>
                 </Switch>
             </Router>
