0f9cc7fe2432ae107e589d09d9752feef8efe2f1
Maya Rachmani <maya3rm@gmail.com>
2020-05-03 10:48:53 +0300

    added http request

--- src/views/Servers/Servers.js
+++ src/views/Servers/Servers.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 export default function Servers() {
+    const [servers, setServers] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/v1/shavit/resources/servers').then((response)=>{
+            setServers(response.data.servers.map((server)=> {
+                return {
+                    key: JSON.parse(server).name,
+                    title : JSON.parse(server).name
+                };
+            }));
+        }).catch((err)=>{
+            alert(err);
+        })
+    });
+
     return(
         <h1>hi</h1>
     )
